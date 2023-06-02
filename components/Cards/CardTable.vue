fcfc5411ff25511b5f20845228d120c83411ae2c
codesyariah122 <pujiermanto@gmail.com>
2023-06-02 18:37:09 +0700

    update user donation delete and restore

--- components/Cards/CardTable.vue
+++ components/Cards/CardTable.vue
@@ -13,6 +13,13 @@
             {{ title }}
           </h3>
         </div>
+        <div>
+          <button type="button" @click="
+            $router.push({
+              path: `/dashboard/${queryMiddle}/add-user`
+            })
+          " class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><i class="fa-solid fa-plus"></i> Add</button>
+        </div>
         <div v-if="!queryParam">
           <button type="button" @click="total > 0 ?
           $router.push({
