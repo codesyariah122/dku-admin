78c09a54b308ea9762bcd93b1aa7f66e1460a31a
codesyariah122 <pujiermanto@gmail.com>
2023-06-05 18:22:42 +0700

    remove add and options table role user

--- components/Cards/CardTable.vue
+++ components/Cards/CardTable.vue
@@ -10,17 +10,17 @@
             class="font-semibold text-lg"
             :class="[color === 'light' ? 'text-blueGray-700' : 'text-white']"
           >
-            {{ title }}
+            {{ title }}  
           </h3>
         </div>
-        <div>
+        <div v-if="types !== 'user-role'">
           <button type="button" @click="
             $router.push({
               path: `/dashboard/${queryMiddle}/add`
             })
           " class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><i class="fa-solid fa-plus"></i> Add</button>
         </div>
-        <div v-if="!queryParam">
+        <div v-if="!queryParam && types !== 'user-role'">
           <button type="button" @click="total > 0 ?
           $router.push({
             path: `/dashboard/${queryMiddle}/trash`,
