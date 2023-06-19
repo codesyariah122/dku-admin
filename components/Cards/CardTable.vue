6a4391ec535be40a3395f8f4f65758dad09bb330
codesyariah122 <pujiermanto@gmail.com>
2023-06-19 13:04:49 +0700

    update cardtables types

--- components/Cards/CardTable.vue
+++ components/Cards/CardTable.vue
@@ -54,18 +54,20 @@
         <molecules-success-alert :success="success" :messageAlert="messageAlert" @close-alert="closeSuccessAlert"/>
       </div>
 
-      <div class="flex justify-start mt-4 mb-4">
-        <div>
-          <h2 class="text-white text-md font-bold">Filter Campaign By</h2>
+      <div v-if="types === 'campaign-data'">
+        <div class="flex justify-start mt-4 mb-4">
+          <div>
+            <h2 class="text-white text-md font-bold">Filter Campaign By</h2>
+          </div>
         </div>
-      </div>
 
-      <div class="flex justify-start mt-6 mb-6">
-        <div>
-          <button @click="resetFilter" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Reset Filter
-            <i class="fa-solid fa-repeat"></i>
-          </button>
+        <div class="flex justify-start mt-6 mb-6">
+          <div>
+            <button @click="resetFilter" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Reset Filter
+              <i class="fa-solid fa-repeat"></i>
+            </button>
+          </div>
         </div>
       </div>
 
